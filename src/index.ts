9edc3c0466b9c9443a10625f8c13a1b1daa0ec5f
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-02-25 23:44:58 +0900

    add path parameter sample

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 app.get('/', (c) => c.text("hello 🔥"))
+app.get('/user/:name', (c) => {
+    const name = c.req.param('name')
+    return c.json({ name: name })
+})
 
 export default app;

39ee13f13485ca97a8e5ab9ecff277499e8db359
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-02-25 23:51:31 +0900

    add parameter

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 app.get('/', (c) => c.text("hello 🔥"))
+
 app.get('/user/:name', (c) => {
     const name = c.req.param('name')
     return c.json({ name: name })
+})
+
+app.get('/animal/:type?', (c) => {
+    const id = c.req.param('type');
+    return c.text('Animal: ' + String(id))
 })
 
 export default app;
